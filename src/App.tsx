11fce47b6d107a6fd3d9d075aeca636c3c7385de
Guilherme Mattos <guilhermevsmattos@outlook.com>
2023-01-19 12:53:38 -0300

    creating summary table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import './styles/global.css';
 import { Header } from './components/Header';
-
-// import { Habit } from "./components/Habit";
-
+import { SummaryTable } from './components/SummaryTable';
 
 export function App() {
 
@@ -11,6 +9,7 @@ export function App() {
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         
         <Header />
+        <SummaryTable />
       
       </div>
     </div>
